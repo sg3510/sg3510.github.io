e986e08ce9effc67f72601edb19d5b3b4fcec820
Seb <sebgrubb@gmail.com>
2026-01-15 17:41:58 -0500

    react-markdown support

---
+++ public/content/interests.md
@@ -0,0 +1,10 @@
+# Interests
+
+I usually try to stay open-minded and curious about various things, but areas that I'm particularly interested in are:
+
+- **AI Dev Tooling:** When building Hedge I taught myself Rust and got back into coding just before LLMs became popular and have had even more fun coding with all of the new tools (eg coding clis) that are accelerating my coding productivity.
+- **Fitness / Health:** Not just regularly exercising but how we can stay healthy for longer as we age. I enjoy fitness monitoring but often question the value of over self-quantification without concrete results.
+- **Medicine:** Having undergone [ankle transplant surgery](http://drbugbee.com/bipolarAnkleAllograft.html), a rare procedure few surgeons I talked to knew about, I'm deeply interested in how doctors make decisions for their patients and how we can help patients better understand their options when doctors themselves don't know all the possibilities. I'll usually have a lot of fun going through all the papers related to the treatment of a condition to understand how the doctors came to agree on the best way forward. LLMs make this even easier.
+- **Hardware:** I studied electrical & electronic engineering mostly because I like to play with and understand what software runs on. I occasionally hack some hardware projects together, such as self-balancing controllers.
+- **Music:** I actually enjoy a wide range, from classical to electronic music. I used to play flute and piano as a kid but have definitely gotten very rusty now.
+- **Video Games:** While I used to play a lot as a kid, I occasionally enjoy trying out the latest games to see how far we've come. Most recently I've been enjoying Zelda: Breath of the Wild and its sequel, Tears of the Kingdom. I also like following efforts to beat video games I'd play as a kid with reinforcement learning (e.g., [Pokemon Red Experiments](https://github.com/PWhiddy/PokemonRedExperiments)) - we've come a long way!
--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,10 @@ import { TypeAnimation } from "react-type-animation";
 import { SocialIcon } from "react-social-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileAlt } from "@fortawesome/free-solid-svg-icons";
+import ReactMarkdown from "react-markdown";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { ThemeToggle } from "./components/ThemeToggle";
+import { useMarkdown } from "./hooks/useMarkdown";
 import "./App.css";
 
 const imageList = [
@@ -31,6 +33,7 @@ const getRandomImage = (current: string) => {
 
 const Home: React.FC = () => {
   const [profileImg, setProfileImg] = useState<string>("seb_hiking.jpg");
+  const { content, loading } = useMarkdown("/content/home.md");
 
   // For 2x images, use srcSet for better quality on retina screens
   const getImgSrc = (img: string) => `/images/${img}`;
@@ -67,16 +70,11 @@ const Home: React.FC = () => {
             onClick={handleImgClick}
           />
         </div>
-        <h1>Hi I'm Seb</h1>
-        <p>I'm currently exploring different startup ideas. My main interests are  solving hard problems with technical products but more specifically in the areas of AI, health and consumer products.</p>
-
-        <p>I enjoy both writing software but also making it scale to millions (or billions) of users - this has led me to jump between the roles of software engineer and a PM at various companies including:</p>
-        <ul>
-        <li><strong>Hedge Labs:</strong> I started a collateralised lending product (Hedge Protocol) and pivoted to a LLM-powered food logging app (Amino).</li>
-          <li><strong>Google:</strong> I joined as part of the APM program and worked on various products including YouTube, Android and Pixel.</li>
-          <li><strong>Home Automation Startup:</strong> Shortly after graduating I spent a year working as a machine learning engineer building sound recognition neural networks for a home automation device.</li>
-          <li><strong>ARM:</strong> I interned there for two summers as a software engineer and a PM, mostly working on the Cortex-M processors.</li>
-        </ul>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <ReactMarkdown>{content}</ReactMarkdown>
+        )}
         <div className="social-icons">
           <SocialIcon
             url="https://twitter.com/seb_uk"
@@ -111,31 +109,31 @@ const Home: React.FC = () => {
   );
 };
 
-const Interests: React.FC = () => (
-  <div className="content-container">
-    <div className="text-content">
-      <h2>Interests</h2>
-      <p>I usually try to stay open-minded and curious about various things, but areas that I'm particularly interested in are:</p>
-      <ul>
-        <li>
-          <strong>Fitness / Health:</strong> Not just regularly exercising but how we can stay healthy for longer as we age. I enjoy fitness monitoring but often question the value of over self-quantification without concrete results.
-        </li>
-        <li>
-          <strong>Medicine:</strong> Having undergone <a href="http://drbugbee.com/bipolarAnkleAllograft.html" target="_blank" rel="noopener noreferrer">ankle transplant surgery</a>, a rare procedure few surgeons I talked to knew about, I'm deeply interested in how doctors make decisions for their patients and how we can help patients better understand their options when doctors themselves don't know all the possibilities. I'll usually have a lot of fun going through all the papers related to the treatment of a condition to understand how the doctors came to agree on the best way forward. LLMs make this even easier.
-        </li>
-        <li>
-          <strong>Hardware:</strong> I studied electrical & electronic engineering mostly because I like to play with and understand what software runs on. I occasionally hack some hardware projects together, such as self-balancing controllers.
-        </li>
-        <li>
-          <strong>Music:</strong> I actually enjoy a wide range, from classical to electronic music. I used to play flute and piano as a kid but have definitely gotten very rusty now.
-        </li>
-        <li>
-          <strong>Video Games:</strong> While I used to play a lot as a kid, I occasionally enjoy trying out the latest games to see how far we've come. Most recently I've been enjoying Zelda: Breath of the Wild and its sequel, Tears of the Kingdom. I also like following efforts to beat video games I'd play as a kid with reinforcement learning (e.g., <a href="https://github.com/PWhiddy/PokemonRedExperiments" target="_blank" rel="noopener noreferrer">Pokemon Red Experiments</a>) - we've come a long way!
-        </li>
-      </ul>
+const Interests: React.FC = () => {
+  const { content, loading } = useMarkdown("/content/interests.md");
+
+  return (
+    <div className="content-container">
+      <div className="text-content">
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <ReactMarkdown
+            components={{
+              a: ({ href, children }) => (
+                <a href={href} target="_blank" rel="noopener noreferrer">
+                  {children}
+                </a>
+              ),
+            }}
+          >
+            {content}
+          </ReactMarkdown>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 interface NavLinkProps {
   to: string;
@@ -197,26 +195,30 @@ const App: React.FC = () => {
   );
 };
 
-function FunThings() {
+const FunThings: React.FC = () => {
+  const { content, loading } = useMarkdown("/content/fun-things.md");
+
   return (
     <div className="content-container">
       <div className="text-content">
-        <h2>Fun Things</h2>
-        <ul>
-          <li>
-            <a
-              href="https://sebgrubb.com/pokemon-tileset/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Pokemon Tileset Viewer
-            </a>
-            : A fun project I made to read and display tilesets and blocksets from Pokemon Yellow in a browser.  In Pokemon, each map is made of a grid of 16x16 pixel squares. Each square is composed of four 8x8 pixel tiles. This was done since Pokemon Yellow had to fit in *just* 503 KB of ROM so devs had to get very creative with how they stored the graphics data.
-          </li>
-        </ul>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <ReactMarkdown
+            components={{
+              a: ({ href, children }) => (
+                <a href={href} target="_blank" rel="noopener noreferrer">
+                  {children}
+                </a>
+              ),
+            }}
+          >
+            {content}
+          </ReactMarkdown>
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default App;
